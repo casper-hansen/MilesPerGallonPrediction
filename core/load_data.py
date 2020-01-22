7e2faa4889c76f536172ebfc630b55ca893911d1
casperbh96 <casperbh.96@gmail.com>
2020-01-22 20:07:57 +0100

    Moved columns to init

--- core/load_data.py
+++ core/load_data.py
@@ -2,18 +2,18 @@ import pandas as pd
 
 class LoadData():
     def __init__(self):
-        pass
+        self.columns = ['mpg',
+                        'cylinders',
+                        'displacement',
+                        'horsepower',
+                        'weight',
+                        'acceleration',
+                        'model_year',
+                        'origin',
+                        'car_name']
 
     def load_dataset_as_df(self):
         df = pd.read_table("data/auto-mpg.data", header=None, delim_whitespace=True)
-        df.columns = ['mpg',
-                      'cylinders',
-                      'displacement',
-                      'horsepower',
-                      'weight',
-                      'acceleration',
-                      'model_year',
-                      'origin',
-                      'car_name']
+        df.columns = self.columns
 
         return df
