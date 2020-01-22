83ee54cecb3e53cc58aa1fc2430f97b2721888d2
casperbh96 <casperbh.96@gmail.com>
2020-01-22 20:15:47 +0100

    Split dataset into training and testing

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+from sklearn.model_selection import train_test_split
+
 from core.clean_data import CleanData
 from core.predict_data import PredictData
 from core.load_data import LoadData
@@ -8,3 +10,13 @@ predicter = PredictData()
 
 df = loader.load_dataset_as_df()
 df = cleaner.clear_question_marks(df)
+
+y = df['mpg']
+X = df.drop(['mpg'], axis=1)
+
+X_train, X_test, y_train, y_test = train_test_split(
+                                    X, y, 
+                                    test_size=0.2, 
+                                    random_state=42
+                                   )
+
