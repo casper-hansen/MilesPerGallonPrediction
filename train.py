70ded9a1ae21ff7e74a71a92df3007de79740db1
casperbh96 <casperbh.96@gmail.com>
2020-01-22 20:19:30 +0100

    Made a model and scored it

--- train.py
+++ train.py
@@ -1,4 +1,6 @@
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
 
 from core.clean_data import CleanData
 from core.predict_data import PredictData
@@ -12,7 +14,7 @@ df = loader.load_dataset_as_df()
 df = cleaner.clear_question_marks(df)
 
 y = df['mpg']
-X = df.drop(['mpg'], axis=1)
+X = df.drop(['mpg', 'car_name'], axis=1)
 
 X_train, X_test, y_train, y_test = train_test_split(
                                     X, y, 
@@ -20,3 +22,10 @@ X_train, X_test, y_train, y_test = train_test_split(
                                     random_state=42
                                    )
 
+rf = RandomForestRegressor()
+rf.fit(X_train, y_train)
+
+pred = rf.predict(X_test)
+score = r2_score(y_test, pred)
+
+print(score)
