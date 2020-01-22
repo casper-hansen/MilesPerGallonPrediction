43c9fa7b5e56df258737c14d4518fa93a235a172
casperbh96 <casperbh.96@gmail.com>
2020-01-22 19:10:43 +0100

    Added Flask hello world

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from core.clean_data import CleanData
 from core.predict_data import PredictData
 
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
